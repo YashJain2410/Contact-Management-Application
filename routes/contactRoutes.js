6d02859cc5fab9e5a6221d79c2b01b858f71d69b
YashJain2410 <137500019+YashJain2410@users.noreply.github.com>
2025-01-07 13:11:48 +0530

    CRUD routes setup

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,4 +5,20 @@ router.route("/").get((req, res) => {
     res.status(200).send("Get all contacts");
 });
 
+router.route("/").post((req, res) => {
+    res.status(200).send("Create contacts");
+});
+
+router.route("/:id").get((req, res) => {
+    res.status(200).send({ message: `Get contact for ${req.params.id}`});
+});
+
+router.route("/:id").put((req, res) => {
+    res.status(200).send({ message: `Update contact for ${req.params.id}`});
+});
+
+router.route("/:id").delete((req, res) => {
+    res.status(200).send({ message: `Delete contact for ${req.params.id}`});
+});
+
 module.exports = router;
